f455963cda4cce64782792ed4d7b3f9e6df37eb4
Bayu Prayudha <bpray32@gmail.com>
2014-01-26 02:12:08 +0700

    initial commit

---
+++ challenges/158_didactic_xor_cipher_2.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+cipher = "948881859781c4979186898d90c4c68c85878f85808b8b808881c6c4828b96c4908c8d97c4878c858888818a8381"
+
+len = cipher.length / 2
+
+# brute-force the key
+(0..255).each { |k|
+  tmp = ''
+  (0..len-1).each { |x|
+    dec = (k ^ cipher[x+x,2].hex)
+    tmp += dec.chr if dec >= 32 and dec <= 126
+  }
+  puts "#{k} : #{tmp}" if / /.match(tmp)
+}
