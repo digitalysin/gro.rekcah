4b95d81162f9643b35917dd78aa7065579f6698a
Bayu Prayudha <bprayudha@users.noreply.github.com>
2014-01-27 15:29:17 +0700

    initial commit

--- challenges/160_didactic_xor_long_cipher.rb
+++ challenges/160_didactic_xor_long_cipher.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/ruby
 
-cipher = '8776459cf37d459fbb7b5ecfbb7f5fcfb23e478aaa3e4389f378439aa13e4e96a77b5fc1f358439df36a4486a03e4381b63e5580a66c0c8ebd6d5b8aa13e459cf34e4d9fa67f02cf90714288a17f589abf7f5886bc705fcfbc700c96bc6b5ecfb7775f8cbc68499daa3f
-'
+cipher = '8776459cf37d459fbb7b5ecfbb7f5fcfb23e478aaa3e4389f378439aa13e4e96a77b5fc1f358439df36a4486a03e4381b63e5580a66c0c8ebd6d5b8aa13e459cf34e4d9fa67f02cf90714288a17f589abf7f5886bc705fcfbc700c96bc6b5ecfb7775f8cbc68499daa3f'
 
 len = cipher.length / 2
 
@@ -9,8 +8,6 @@ list = Array[0x87,0x76,0x45,0x9C]
 keys = Array[0x00,0x00,0x00,0x00]
 s = ['T','h','i','s']
 str = ''
-
-
 
 # find 4 bytes key
 list.each_with_index { |x,y|
@@ -31,5 +28,4 @@ list.each_with_index { |x,y|
   str += str1 + str2 + str3 + str4
 }
 
-puts str
-
+print str
