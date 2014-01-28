ac7ce9cd76a35dafe60ca62369c83c9ea40753c7
Bayu Prayudha <bpray32@gmail.com>
2014-01-28 15:45:39 +0700

    initial commit

--- challenges/115_delegates.rb
+++ challenges/115_delegates.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/ruby
 
-total = 0
-
 def is_perfect_square(num)
   return true if Math.sqrt(num) - (Math.sqrt(num)).to_i == 0
   false
 end
+
+total = 0
 
 (0..2118).each do |x|
   if is_perfect_square(x)
